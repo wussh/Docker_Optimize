bce787b8ae896302bac4609891bc336c3073e0c6
wussh <andrianta.321@gmail.com>
2024-04-24 10:41:04 +0700

    Initial commit

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+)
+
+func main() {
+	e := echo.New()
+
+	e.GET("/", HomeHandler)
+
+	e.Logger.Fatal(e.Start(":8080"))
+}
+
+func HomeHandler(c echo.Context) error {
+	return c.String(http.StatusOK, "Hello, World!")
+}
